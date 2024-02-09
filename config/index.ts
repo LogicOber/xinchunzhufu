b2fa18c71f9883706e7b4b526b419399199baa67
LogicOber <133167749+LogicOber@users.noreply.github.com>
2024-02-09 22:30:32 +0800

    init

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: '新春祝福语生成器',
+  description: '👉 输入要祝福的人以及和你的关系，直接生成祝福语～',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
